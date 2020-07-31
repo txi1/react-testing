7c5fa6a2bbced79ce509ebe26aaaf2219f528d54
Taylor Xi <caetortaylor@gmail.com>
2020-07-31 19:33:45 -0400

    Updates

---
+++ precache-manifest.5db5fa6b236d1eacf6e116046b329784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80173566f7f7857570709922e76e1297",
+    "url": "/react-basics/index.html"
+  },
+  {
+    "revision": "9f76fdb210dd284c5699",
+    "url": "/react-basics/static/css/main.be126900.chunk.css"
+  },
+  {
+    "revision": "54a0e2ccd94441be5863",
+    "url": "/react-basics/static/js/2.9567f230.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics/static/js/2.9567f230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f76fdb210dd284c5699",
+    "url": "/react-basics/static/js/main.bfeed791.chunk.js"
+  },
+  {
+    "revision": "ecb067e5745a49c636d5",
+    "url": "/react-basics/static/js/runtime-main.74e35950.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-basics/static/media/logo.5d5d9eef.svg"
+  }
+]);
